1c1b974a2eea9f658250c4ec39c6f8e0ddd0b3d4
GersonVis <visoso126@gmail.com>
2023-01-26 23:38:02 -0600

    login

--- src/main/java/com/example/wildproject/LoginActivity.kt
+++ src/main/java/com/example/wildproject/LoginActivity.kt
@@ -2,10 +2,35 @@ package com.example.wildproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import com.example.wildproject.databinding.ActivityLoginBinding
+import com.example.wildproject.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.properties.Delegates
 
 class LoginActivity : AppCompatActivity() {
+    companion object{
+        lateinit var useremail: String
+        lateinit var providerSession: String
+    }
+    private var email by Delegates.notNull<String>()
+    private var password by Delegates.notNull<String>()
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var lyTerms: LinearLayout
+
+    private lateinit var mAuth: FirebaseAuth
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lyTerm.visibility= View.INVISIBLE
     }
 }
